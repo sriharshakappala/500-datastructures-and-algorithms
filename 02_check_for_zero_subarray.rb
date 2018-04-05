7773e95be302bf4340fb9e20b1a900f371e76454
Sri Harsha Kappala <sriharsha.k@caratlane.com>
2018-04-05 23:57:01 +0530

    Find subarray with 0 sum

---
+++ 02_check_for_zero_subarray.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'byebug'
+
+def zeroSubArray array, sum
+  counterHash = {}
+  current_sum = 0
+  array.each_with_index do |item, index|
+    # byebug
+    current_sum += item
+    if current_sum == sum
+      puts "Sum found between indexes 0 and #{index}"
+      return true
+    end
+    if counterHash.key? (current_sum - sum)
+      puts "Sum found between indexes #{counterHash[current_sum - sum] + 1} and #{index}"
+      return true
+    end
+    counterHash[current_sum] = index
+  end
+  puts "No subarray exists with given sum"
+  return false
+end
+
+zeroSubArray [3, 4, -7, 3, 1, 3, 1, -4, -2, -2], 0
